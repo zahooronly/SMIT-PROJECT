ca439a482407d926492226e96422b2d77d6c266c
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-06 20:45:02 +0500

    route.ts

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -1,4 +1,4 @@
-import { connected } from "@/db/dbConfig";
+import { connected } from "@/config/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
